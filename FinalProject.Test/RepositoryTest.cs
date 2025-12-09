d7543545537628a3f574531b5f1a33cef12fda6a
sanchess2803 <88556220+mateuscostasanches@users.noreply.github.com>
2025-12-09 20:55:54 -0300

    100% Test

---
+++ FinalProject.Test/RepositoryTest.cs
@@ -0,0 +1,141 @@
+﻿
+using System.Text.Json;
+using FinalProject.Domain.Entities;
+using FinalProject.Repository.Context;
+
+namespace FinalProject.Test
+{
+    [TestClass]
+    public sealed class RepositoryTest
+    {
+
+        [TestMethod]
+        public void TestAddress()
+        {
+            using (var context = new FinalProjectContext())
+            {
+                Console.WriteLine("Connected!");
+
+                var address1 = new Address(1, "Sao Paulo", "Buritama");
+                var address2 = new Address(2, "Sao Paulo", "Aracatuba");
+                var address3 = new Address(3, "California", "Los Angeles");
+
+                context.Addresses.Add(address1);
+                context.Addresses.Add(address2);
+                context.Addresses.Add(address3);
+
+                context.SaveChanges();
+                Console.WriteLine("Insert Done!");
+
+                foreach (var cat in context.Addresses)
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(cat));
+                }
+                Console.WriteLine("Address List Done!");
+            }
+        }
+
+        [TestMethod]
+        public void TestCategory()
+        {
+            using (var context = new FinalProjectContext())
+            {
+                Console.WriteLine("Connected!");
+                var category1 = new Category(1, "Indie");
+                var category2 = new Category(2, "Soulslike");
+                var category3 = new Category(3, "Casual");
+
+                context.Categories.Add(category1);
+                context.Categories.Add(category2);
+                context.Categories.Add(category3);
+
+                context.SaveChanges();
+                Console.WriteLine("Insert Done!");
+
+                foreach (var cat in context.Categories)
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(cat));
+                }
+                Console.WriteLine("Category List Done!");
+            }
+
+        }
+
+        [TestMethod]
+        public void TestCustomer()
+        {
+            using (var context = new FinalProjectContext())
+            {
+                Console.WriteLine("Connected!");
+                var address4 = new Address(4, "Sao Paulo", "Valparaiso");
+                var address5 = new Address(5, "Sao Paulo", "Brejo Alegre");
+                var customer1 = new Customer(1, "Renan Avelino", "renanzinhogamer@gmail.com", "senha123@", address4, DateTime.Now.Date);
+                var customer2 = new Customer(2, "Flavio Grigas", "eogrigasne@grigas.com", "irmaodoigor12", address5, DateTime.Now.Date);
+
+                context.Addresses.Add(address4);
+                context.Addresses.Add(address5);
+                context.Customers.Add(customer1);
+                context.Customers.Add(customer2);
+                context.SaveChanges();
+                Console.WriteLine("Insert Done!");
+
+                foreach (var cat in context.Customers)
+                {
+                    Console.Write(JsonSerializer.Serialize(cat));
+                }
+                Console.WriteLine("Customer List Done!");
+            }
+
+        }
+
+        [TestMethod]
+        public void TestDeveloper()
+        {
+            using (var context = new FinalProjectContext())
+            {
+                var address = new Address(6, "Sao Paulo", "Sao Paulo");
+                var developer = new Developer(2, "Toca do Page", "indianistas@gmail.com", "tocadopage123", address, DateTime.Now.Date);
+
+                context.Addresses.Add(address);
+                context.Developers.Add(developer);
+                context.SaveChanges();
+                Console.WriteLine("Insert Done!");
+
+                foreach (var cat in context.Developers)
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(cat));
+                }
+                Console.WriteLine("Developer List Done!");
+            }
+
+        }
+
+        [TestMethod]
+        public void TestProduct()
+        {
+            using (var context = new FinalProjectContext())
+            {
+                var category = new Category(4, "RPG");
+                var address = new Address(7, "Sao Paulo", "Sao Paulo");
+                var developer = new Developer(1, "Dumativa", "dumativa@dumativa.com", "dumativagames123", address, DateTime.Now.Date);
+                var product = new Product(1, "Enigma do Medo", "RPG Horror Game", 59.99m, DateTime.Now.Date, category, developer );
+
+                context.Categories.Add(category);
+                context.Addresses.Add(address);
+                context.Developers.Add(developer);
+                context.Products.Add(product);
+                context.SaveChanges();
+                Console.WriteLine("Insert Done!");
+
+                foreach (var cat in context.Developers)
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(cat));
+                }
+                Console.WriteLine("Product List Done!");
+            }
+
+        }
+
+    }
+
+}
---
+++ FinalProject.Test/ServiceTest.cs
@@ -0,0 +1,181 @@
+﻿
+using AutoMapper;
+using System.Text.Json;
+using FinalProject.Domain.Base;
+using FinalProject.Domain.Entities;
+using FinalProject.Service.Service;
+using FinalProject.Service.Validators;
+using FinalProject.Repository.Context;
+using FinalProject.Repository.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
+
+
+namespace FinalProject.Test
+{
+
+    [TestClass]
+    public sealed class ServiceTest
+    {
+        private ServiceCollection? services;
+
+        #region Configurations of Services
+        public ServiceProvider ConfigureServices()
+        {
+            services = new ServiceCollection();
+            services.AddDbContext<FinalProjectContext>();
+            services.AddScoped<IBaseRepository<Address>, BaseRepository<Address>>();
+            services.AddScoped<IBaseRepository<Category>, BaseRepository<Category>>();
+            services.AddScoped<IBaseRepository<Customer>, BaseRepository<Customer>>();
+            services.AddScoped<IBaseRepository<Developer>, BaseRepository<Developer>>();
+            services.AddScoped<IBaseRepository<Product>, BaseRepository<Product>>();
+            services.AddScoped<IBaseRepository<SaleItem>, BaseRepository<SaleItem>>();
+            services.AddScoped<IBaseRepository<Sale>, BaseRepository<Sale>>();
+
+
+            services.AddScoped<IBaseService<Address>, BaseService<Address>>();
+            services.AddScoped<IBaseService<Category>, BaseService<Category>>();
+            services.AddScoped<IBaseService<Customer>, BaseService<Customer>>();
+            services.AddScoped<IBaseService<Developer>, BaseService<Developer>>();
+            services.AddScoped<IBaseService<Product>, BaseService<Product>>();
+            services.AddScoped<IBaseService<SaleItem>, BaseService<SaleItem>>();
+            services.AddScoped<IBaseService<Sale>, BaseService<Sale>>();
+
+            services.AddSingleton(
+              new MapperConfiguration(
+                config => {
+                    config.CreateMap<Address, Address>();
+                    config.CreateMap<Category, Category>();
+                    config.CreateMap<Customer, Customer>();
+                    config.CreateMap<Developer, Developer>();
+                    config.CreateMap<Product, Product>();
+                    config.CreateMap<SaleItem, SaleItem>();
+                    config.CreateMap<Sale, Sale>();
+                }, NullLoggerFactory.Instance).CreateMapper()
+              );
+            return services.BuildServiceProvider();
+        }
+        #endregion
+
+        #region Test Service Method
+        [TestMethod]
+        public void TestFinalProjectService()
+        {
+            var serviceProvider = ConfigureServices();
+
+            #region AddressServiceTest
+            var addressService = serviceProvider.GetService<IBaseService<Address>>();
+            var address1 = new Address
+            {
+                City = "Mongaqua",
+                State = "Sao Paulo"
+            };
+            var address2 = new Address
+            {
+                City = "Los Angeles",
+                State = "California"
+            };
+            var addedAddress1 = addressService!.Add<Address, Address, AddressValidator>(address1);
+            var addedAddress2 = addressService!.Add<Address, Address, AddressValidator>(address2);
+
+            var allAddress = addressService.Get<Address>();
+            if (allAddress != null)
+            {
+                foreach (var c in allAddress)
+                {
+                    Console.WriteLine("Address: " + JsonSerializer.Serialize(c));
+                }
+            }
+            #endregion
+
+            #region CategoryServiceTest
+            var categoryService = serviceProvider.GetService<IBaseService<Category>>();
+            var category = new Category
+            {
+                Name = "Multiplayer"
+            };
+            var addedCategory = categoryService!.Add<Category, Category, CategoryValidator>(category);
+
+            var allCategories = categoryService.Get<Category>();
+            if (allCategories != null)
+            {
+                foreach (var c in allCategories)
+                {
+                    Console.WriteLine("Categories: " + JsonSerializer.Serialize(c));
+                }
+            }
+            #endregion
+
+            #region CustomerServiceTest
+            var customerService = serviceProvider.GetService<IBaseService<Customer>>();
+            var customer = new Customer
+            {
+                Name = "Bruno Lara",
+                Email = "brunolara123@gmail.com",
+                Password = "Mijodeindio@456",
+                RegisterDate = DateTime.Now,
+                Address = address1 
+            };
+            var addedCustomer = customerService!.Add<Customer, Customer, CustomerValidator>(customer);
+
+            var allCustomers = customerService.Get<Customer>();
+            if (allCustomers != null)
+            {
+                foreach (var c in allCustomers)
+                {
+                    Console.WriteLine("Customers: " + JsonSerializer.Serialize(c));
+                }
+            }
+            #endregion
+
+            #region DeveloperServiceTest
+            var developerService = serviceProvider.GetService<IBaseService<Developer>>();
+            var developer = new Developer
+            {
+                Name = "Valve",
+                Email = "valvesuport@gmail.com",
+                Password = "ValveandSteamisfuck$123",
+                RegisterDate = DateTime.Now,
+                Address = address2 
+            };
+            var addedDeveloper = developerService!.Add<Developer, Developer, DeveloperValidator>(developer);
+
+            var allDevelopers = developerService.Get<Developer>();
+            if (allDevelopers != null)
+            {
+                foreach (var c in allDevelopers)
+                {
+                    Console.WriteLine("Developers: " + JsonSerializer.Serialize(c));
+                }
+            }
+            #endregion
+
+            #region ProductServiceTest
+            var productService = serviceProvider.GetService<IBaseService<Product>>();
+            var product = new Product
+            {
+                Name = "Counter Strike 2",
+                Description = "Shoot in 3th person multiplayer",
+                Category = category,
+                Developer = developer,
+                ReleaseDate = DateTime.Now,
+                Price = 59.99m
+            };
+            var addedProduct = productService!.Add<Product, Product, ProductValidator>(product);
+
+            var allProducts = productService.Get<Product>();
+            if (allProducts != null)
+            {
+                foreach (var c in allProducts)
+                {
+                    Console.WriteLine("Products: " + JsonSerializer.Serialize(c));
+                }
+            }
+            #endregion
+
+        }
+        #endregion
+
+    }
+
+}
